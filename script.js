b46cc7eec901770fec26c1cc6aefbae01d2e5865
vetrivel-777 <rsvetrivel666@gmail.com>
2026-05-04 11:56:46 +0530

    Initial local project files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const display = document.getElementById("display");
 const buttons = document.querySelectorAll("button");
 
@@ -19,4 +20,31 @@ buttons.forEach(button => {
 
         display.value = currentInput;
     });
+=======
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.innerText;
+
+        if (value === "C") {
+    currentInput = "";
+} 
+else if (value === "=") {
+    try {
+        currentInput = eval(currentInput);
+    } catch {
+        currentInput = "Error";
+    }
+} 
+else {
+    currentInput += value;
+}
+
+        display.value = currentInput;
+    });
+>>>>>>> 6d6cd42 (Initial local project files)
 });
